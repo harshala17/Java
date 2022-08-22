4e57d6eefccac957ecbb88d2f2f5ca738ceb6a61
Harshala Athani <harshalaathani@gmail.com>
2022-08-23 00:08:07 +0530

    Added a Queue,java file

---
+++ Queue.java
@@ -0,0 +1,49 @@
+public class Queue {
+    static class QueueE{
+        static int arr[];
+        static int size;
+        static int rear=-1;
+
+        //Queue Constructor..
+        QueueE(int n){
+            arr=new int[n]; //this is for array size.
+            this.size=n; //to store size variable..
+        }
+        public static boolean isEmpty(){
+             return rear==-1;
+        }
+        //add
+        public static void add(int data){
+            if (rear==size-1){
+                System.out.println("Queue is full..");
+                return;
+            }
+            rear++;
+            arr[rear]=data;
+        }
+
+        public static int remove(){
+            if (isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int front=arr[0];
+            for (int i =0;i<rear;i++){
+                arr[i]=arr[i+1];
+            }
+            rear--;
+            return front;
+
+        }
+    }
+    public static void main(String[] args) {
+    QueueE queue=new QueueE(5);
+    queue.add(1);
+    queue.add(2);
+    queue.add(3);
+
+
+
+    }
+
+}
